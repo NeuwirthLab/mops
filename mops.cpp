e4239e565619f6658ac88eb715d0fc81ae7b3366
Zhaobin Zhu <zhu.zhaobin@hotmail.de>
2024-04-18 11:26:04 +0200

    argpaser

--- mops.cpp
+++ mops.cpp
@@ -12,25 +12,42 @@ namespace fs = std::filesystem;
 using ValueType = double;
 using Clock = std::chrono::high_resolution_clock;
 using Duration = std::chrono::duration<double>;
+static int copy_flag = false;
+static int tacoed = false;
+
 
 int main(int argc, char** argv){
 	fs::path input_file{};
 	fs::path output_file{};
+    int cols, rows;
 	int threads = std::thread::hardware_concurrency();
 
-	const char* const short_opts = "i:o:t:";
+	const char* const short_opts = "i:o:t:c:r";
 
 	static const struct option long_opts[] = {
 	    {"input", required_argument, nullptr, 'i'},
 	    {"output", required_argument, nullptr, 'o'},
 	    {"threads", required_argument, nullptr, 't'},
+        {"cols",    required_argument, nullptr, 'c'},
+        {"rows",    required_argument, nullptr, 'r'},
+        {"copy", no_argument, &copy_flag, true},
+        {"tacoed", no_argument, &tacoed, true},
 	};
 
 	while (true) {
-		const auto opt =
-		    getopt_long(argc, argv, short_opts, long_opts, nullptr);
-		if (opt == -1)
-			break;
+        int option_index = 0;
+        const auto opt =
+                getopt_long(argc, argv, short_opts, long_opts, nullptr);
+        if (opt == -1)
+            break;
+        switch (opt) {
+            case 0:
+                if (long_opts[option_index].flag != nullptr)
+                    break;
+                if (optarg)
+                    printf (" with arg %s", optarg);
+                printf ("\n");
+                break;
 		switch (opt) {
 			case 'i':
 				input_file = optarg;
